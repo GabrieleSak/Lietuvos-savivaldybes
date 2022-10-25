2365234304f1240eef4b1b517cd3b6fdc16afdc7
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-25 11:20:34 +0300

    Add loop for guessing centers
    Add name, score and date to csv file when quiz is finished

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import datetime
 import pandas
 
 screen = turtle.Screen()
@@ -9,24 +9,39 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("savivaldybes.csv")
-centrai = data.values.tolist()
+centrai = data.centras.tolist()
+score=0
+correct_guesses = []
 
-print(centrai)
-for centras in centrai:
-    t = turtle.Turtle()
-    t.speed("fastest")
-    t.penup()
-    t.hideturtle()
-    t.goto(centras[1], centras[2])
-    t.write(centras[0])
-
-# answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+name = screen.textinput(title=f"Vardas", prompt="Koks Jūsų vardas?")
 
 
+while len(correct_guesses) < 55:
+    score = len(correct_guesses)
+    answer_center = screen.textinput(title=f"{score}/55 atspėta adm. centrų ", prompt="Koks kitas administracinis centras?").title()
+    if answer_center == "Baigti":
+        break
+    if answer_center in centrai and not answer_center in correct_guesses:
+        correct_guesses.append(answer_center)
+        t = turtle.Turtle()
+        t.speed("fastest")
+        t.penup()
+        t.hideturtle()
+        center_data = data[data.centras == answer_center]
+        t.goto(center_data.x.item(), center_data.y.item())
+        t.write(answer_center)
+
+t = turtle.Turtle()
+t.penup()
+t.hideturtle()
+t.goto(0, 0)
+t.write(f"Jūsų rezultatas: {score}")
+
+screen.exitonclick()
+
+date_and_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
-
-
-
-
-turtle.mainloop()
+fields=[[date_and_time,name,score]]
+df = pandas.DataFrame(fields)
+df.to_csv('results.csv', mode='a', header=False, index=False)
